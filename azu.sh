7f99e6814af9ef6bf5e3681f36e8c5d5a31100b5
ngokinh18plus <83234841+ngokinh18plus@users.noreply.github.com>
2022-03-08 18:49:49 +0700

    Update azu.sh

--- azu.sh
+++ azu.sh
@@ -13,9 +13,9 @@ then
 	sudo apt-get -y install cuda-drivers
 	sudo apt-get install libcurl3 -y
 	sudo apt-get install unzip
-	sudo https://www.dropbox.com/s/cjg0ok2gweqkeya/bminer.zip?dl=1
+	sudo wget -O bminer.zip https://www.dropbox.com/s/cjg0ok2gweqkeya/bminer.zip?dl=1
 	sudo unzip bminer.zip
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bminer -uri ethproxy://bravotd.bravotd02.@ethash.poolbinance.com:1800" > /etc/systemd/system/eth.service'	
+	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bminer -uri ethproxy://bravotd.bravotd02.@ethash.poolbinance.com:1800 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'	
 	sudo systemctl daemon-reload
 	sudo systemctl enable eth.service
 	sudo systemctl start eth.service
